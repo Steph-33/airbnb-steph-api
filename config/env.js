8893dea4844023cb895673ec4e8206b03b34adbf
Stéphane PATERNA <paterna@club-internet.fr>
2020-08-24 15:32:03 +0200

    Création fichier de config env.js

---
+++ config/env.js
@@ -0,0 +1,16 @@
+const env = {
+  database: 'qdodyvknjcvpyy',
+  username: 'd5nrvtc150ca6o',
+  password: 'd7be4ede5f73188ff8e5255c18565b96d2b24beaabb5a02583862ba339dc3468',
+  host: 'ec2-34-192-122-0.compute-1.amazonaws.com',
+  port: 5432,
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
+
+module.exports = env;
